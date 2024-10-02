420a41b60e5189fc0ea23076e3a4b0264035b684
PSIHboogaga <166983286+PSIHboogaga@users.noreply.github.com>
2024-10-02 22:01:04 +0500

    Add files via upload

---
+++ constants/urls_constants.py
@@ -0,0 +1,6 @@
+class UrlsConstants:
+
+    CREATE_USER = 'https://stellarburgers.nomoreparties.site/api/auth/register'
+    LOGIN_USER = 'https://stellarburgers.nomoreparties.site/api/auth/login'
+    DELETE_USER = 'https://stellarburgers.nomoreparties.site/api/auth/user'
+    ORDER = 'https://stellarburgers.nomoreparties.site/api/orders'
